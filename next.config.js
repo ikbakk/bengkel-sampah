42f7aff62c698d689466a56352784a55fd5394f9
ikbakk <iqbalfirdaus05@gmail.com>
2023-10-15 00:14:31 +0800

    remove secrets from nextconfig into .env

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    BACKEND_URL: "https://silver-doodle-4967rrrxq7xf759r-3000.app.github.dev",
-    NEXTAUTH_SECRET: "awsredtfyguhijok45678",
-    NEXTAUTH_URL: "https://silver-doodle-4967rrrxq7xf759r-3000.app.github.dev",
-    
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
