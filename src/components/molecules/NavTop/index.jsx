08153991be3813a442cbad228c2079b8d9689685
agusdwimilniadi <agusdwimill@gmail.com>
2023-10-22 21:07:02 +0700

    [FE]- Slicing Jome Dashboard Admin

--- src/components/molecules/NavTop/index.jsx
+++ src/components/molecules/NavTop/index.jsx
@@ -4,7 +4,9 @@ import React from "react";
 export const NavTop = ({ label }) => {
   return (
     <div className="flex items-center justify-between  py-10 ">
-      <h1 className="text-3xl font-semibold  text-gray-600">{label}</h1>
+      <h1 className="text-xl font-semibold text-gray-600  lg:text-3xl">
+        {label}
+      </h1>
 
       <div>
         <Image
