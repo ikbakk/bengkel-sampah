2dd7c10417e3f54fac099911d55a7a7cb7efd063
farrely-f <farrely.firenza@outlook.com>
2023-10-23 00:46:48 +0700

    [FE] - Slicing Login and Register Page

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = withMT({
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        primaryimage: "url('/images/bengkel-sampah-bg.png')",
       },
     },
   },
