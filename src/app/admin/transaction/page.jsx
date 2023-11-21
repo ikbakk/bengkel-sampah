16036d33ffb2fe9cf2b43fd0cdee3a62d7231c6d
ikbakk <iqbalfirdaus05@gmail.com>
2023-11-21 19:42:54 +0800

    remove unused vars

--- src/app/admin/transaction/page.jsx
+++ src/app/admin/transaction/page.jsx
@@ -1,20 +1,16 @@
 import React from "react";
 import Transactions from "@/components/templates/Transactions";
-import DaftarSampahTemplate from "@/components/templates/DaftarSampahTemplate";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
 import { fetchItems } from "@/utils/fetchItems";
 
 const TransactionPage = async () => {
   const session = await getServerSession(authOptions);
-  const data = await fetchItems("/api/waste", session.accessToken);
   const transactions = await fetchItems(
     "/api/transactions",
     session.accessToken,
   );
 
-  console.log(transactions);
-  // return <DaftarSampahTemplate data={data.data} />;
   return (
     <>
       <Transactions data={transactions.data} token={session.accessToken} />
